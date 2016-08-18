449ad5b2622a301791fbcae3ea96e3675449b492
Colin Young <cyoung@patientsecure.com>
2016-08-18 15:41:55 -0400

    Integration test to verify database is created with custom column names

---
+++ test/Serilog.Sinks.MSSqlServer.Tests/CustomStandardColumnNames.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Diagnostics;
+using System.Linq;
+using Dapper;
+using Xunit;
+using FluentAssertions;
+
+namespace Serilog.Sinks.MSSqlServer.Tests
+{
+    public class CustomStandardColumnNames : IClassFixture<DatabaseFixture>
+    {
+        public class InfoSchema
+        {
+            public string ColumnName { get; set; }
+        }
+
+        [Fact]
+        public void TableCreatedWithCustomNames()
+        {
+            // arrange
+            var options = new ColumnOptions();
+
+            options.Store.ToList().ForEach(c =>
+            {
+                options.Store.Remove(c.Key);
+                options.Store.Add(c.Key, $"Custom{c.Key.ToString()}");
+            });
+
+            // act
+            var sink = new MSSqlServerSink(DatabaseFixture.LogEventsConnectionString, DatabaseFixture.LogTableName, 1, TimeSpan.FromSeconds(1), null, true, options);
+
+            // assert
+            using (var conn = new SqlConnection(DatabaseFixture.MasterConnectionString))
+            {
+                conn.Execute($"use {DatabaseFixture.Database}");
+                var logEvents = conn.Query<InfoSchema>($@"SELECT COLUMN_NAME AS ColumnName FROM {DatabaseFixture.Database}.INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = '{DatabaseFixture.LogTableName}'");
+                var infoSchemata = logEvents as InfoSchema[] ?? logEvents.ToArray();
+
+                foreach (var column in options.Store.Values)
+                {
+                    Console.WriteLine($"Testing {column}");
+                    infoSchemata.Should().Contain(columns => columns.ColumnName == column);
+                }
+            }
+        }
+
+        [Fact]
+        public void TableCreatedWithDefaultNames()
+        {
+            
+        }
+    }
+}
---
+++ test/Serilog.Sinks.MSSqlServer.Tests/DatabaseFixture.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Linq;
+using Dapper;
+
+namespace Serilog.Sinks.MSSqlServer.Tests
+{
+    public class DatabaseFixture : IDisposable
+    {
+        public static string Database => "LogTest";
+        public static string LogTableName => "LogEvents";
+
+        private const string CreateLogEventsDatabase = @"
+EXEC ('CREATE DATABASE [{0}] ON PRIMARY 
+	(NAME = [{0}], 
+	FILENAME =''{1}'', 
+	SIZE = 25MB, 
+	MAXSIZE = 50MB, 
+	FILEGROWTH = 5MB )')";
+
+        private static readonly string DatabaseFileNameQuery = $@"SELECT CONVERT(VARCHAR(255), SERVERPROPERTY('instancedefaultdatapath')) + '{Database}.mdf' AS Name";
+        private static readonly string DropLogEventsDatabase = $@"
+ALTER DATABASE [{Database}]
+SET SINGLE_USER
+WITH ROLLBACK IMMEDIATE
+DROP DATABASE [{Database}]
+";
+
+        public static string MasterConnectionString => @"Data Source=(LocalDb)\v11.0;Initial Catalog=Master;Integrated Security=True";
+        public static string LogEventsConnectionString => $@"Data Source=(LocalDb)\v11.0;Initial Catalog={Database};Integrated Security=True";
+
+        public class FileName
+        {
+            public string Name { get; set; }
+        }
+
+        public DatabaseFixture()
+        {
+            CreateDatabase();
+        }
+        
+        public void Dispose()
+        {
+            DeleteDatabase();
+        }
+
+        private static void DeleteDatabase()
+        {
+            using (var conn = new SqlConnection(MasterConnectionString))
+            {
+                conn.Open();
+                var databases = conn.Query("select name from sys.databases");
+
+                //if (databases.Any(d => d.name == Database)) conn.Execute(DropLogEventsDatabase);
+            }
+        }
+
+        private static void CreateDatabase()
+        {
+            DeleteDatabase();
+
+            using (var conn = new SqlConnection(MasterConnectionString))
+            {
+                conn.Open();
+                // ReSharper disable once PossibleNullReferenceException
+                var filename = conn.Query<FileName>(DatabaseFileNameQuery).FirstOrDefault().Name;
+                var createDatabase = string.Format(CreateLogEventsDatabase, Database, filename);
+
+                conn.Execute(createDatabase);
+            }
+        }
+    }
+}
