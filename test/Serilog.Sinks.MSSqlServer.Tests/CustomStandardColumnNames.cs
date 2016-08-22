fdc3aaa6f02e2672393d4fa15ad4de0295dd5852
Colin Young <cyoung@patientsecure.com>
2016-08-22 14:02:43 -0400

    Verify default standard event column names are used

--- test/Serilog.Sinks.MSSqlServer.Tests/CustomStandardColumnNames.cs
+++ test/Serilog.Sinks.MSSqlServer.Tests/CustomStandardColumnNames.cs
@@ -1,10 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data.SqlClient;
+using System.IO;
 using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
 using Dapper;
 using Xunit;
 using FluentAssertions;
+using Serilog.Events;
 
 namespace Serilog.Sinks.MSSqlServer.Tests
 {
@@ -111,5 +115,46 @@ namespace Serilog.Sinks.MSSqlServer.Tests
                 }
             }
         }
+
+        [Fact]
+        public void WriteEventToDefaultStandardColumns()
+        {
+            // arrange
+            var loggerConfiguration = new LoggerConfiguration();
+            Log.Logger = loggerConfiguration.WriteTo.MSSqlServer(
+                connectionString: DatabaseFixture.LogEventsConnectionString,
+                tableName: DatabaseFixture.LogTableName,
+                autoCreateSqlTable: true,
+                batchPostingLimit: 1,
+                period: TimeSpan.FromSeconds(10),
+                columnOptions: new ColumnOptions())
+                .CreateLogger();
+
+            var file = File.CreateText("Self.log");
+            Serilog.Debugging.SelfLog.Enable(TextWriter.Synchronized(file));
+
+            // act
+            const string loggingInformationMessage = "Logging Information message";
+            Log.Information(loggingInformationMessage);
+
+            //Thread.Sleep(50);
+
+            Log.CloseAndFlush();
+
+            // assert
+            using (var conn = new SqlConnection(DatabaseFixture.LogEventsConnectionString))
+            {
+                var logEvents = conn.Query<DefaultStandardLogColumns>($"SELECT Message, Level FROM {DatabaseFixture.LogTableName}");
+
+                logEvents.Should().Contain(e => e.Message.Contains(loggingInformationMessage));
+            }
+        }
+    }
+
+    public class DefaultStandardLogColumns
+    {
+        public string Message { get; set; }
+
+        public LogEventLevel Level { get; set; }
     }
 }
