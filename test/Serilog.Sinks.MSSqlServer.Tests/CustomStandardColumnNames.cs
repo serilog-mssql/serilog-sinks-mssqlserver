6e4ee2ce4fec9386ca81f9947dc47272e6723d0e
Colin Young <cyoung@patientsecure.com>
2016-09-14 10:10:36 -0400

    Remove errant debugger launch

--- test/Serilog.Sinks.MSSqlServer.Tests/CustomStandardColumnNames.cs
+++ test/Serilog.Sinks.MSSqlServer.Tests/CustomStandardColumnNames.cs
@@ -120,7 +120,6 @@ namespace Serilog.Sinks.MSSqlServer.Tests
         [Fact]
         public void WriteEventToCustomStandardColumns()
         {
-            Debugger.Launch();
             // arrange
             var options = new ColumnOptions();
 
