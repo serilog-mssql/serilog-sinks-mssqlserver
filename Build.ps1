9243709ca1d9ef9f19fe6b1bb4fc6eadddeb63f4
Nicholas Blumhardt <nblumhardt@nblumhardt.com>
2018-01-04 08:59:38 +1000

    --version-suffix fix

--- Build.ps1
+++ Build.ps1
@@ -18,9 +18,13 @@ echo "build: Version suffix is $suffix"
 foreach ($src in ls src/*) {
     Push-Location $src
 
-	echo "build: Packaging project in $src"
+    echo "build: Packaging project in $src"
 
-    & dotnet pack -c Release -o ..\..\artifacts --version-suffix=$suffix
+    if ($suffix) {
+        & dotnet pack -c Release -o ..\..\artifacts --version-suffix=$suffix
+    } else {
+        & dotnet pack -c Release -o ..\..\artifacts
+    }
     if($LASTEXITCODE -ne 0) { exit 1 }    
 
     Pop-Location
@@ -29,7 +33,7 @@ foreach ($src in ls src/*) {
 foreach ($test in ls test/*.PerformanceTests) {
     Push-Location $test
 
-	echo "build: Building performance test project in $test"
+    echo "build: Building performance test project in $test"
 
     & dotnet build -c Release
     if($LASTEXITCODE -ne 0) { exit 2 }
@@ -40,7 +44,7 @@ foreach ($test in ls test/*.PerformanceTests) {
 foreach ($test in ls test/*.Tests) {
     Push-Location $test
 
-	echo "build: Testing project in $test"
+    echo "build: Testing project in $test"
 
     & dotnet test -c Release
     if($LASTEXITCODE -ne 0) { exit 3 }
