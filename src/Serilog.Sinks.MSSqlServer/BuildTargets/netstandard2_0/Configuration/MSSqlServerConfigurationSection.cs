f216475f6a7cfd42ce00cc0ee57b21c783d11cba
Jon McGuire <mcguirev10@gmail.com>
2018-04-11 12:03:06 -0400

    copy config dependences to separate build-target folders

---
+++ src/Serilog.Sinks.MSSqlServer/BuildTargets/netstandard2_0/Configuration/MSSqlServerConfigurationSection.cs
@@ -0,0 +1,54 @@
+﻿// Copyright 2015 Serilog Contributors
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using System.Configuration;
+
+namespace Serilog.Configuration
+{
+    /// <summary>
+    /// Settings configuration for defining DataColumns collection
+    /// </summary>
+    public class MSSqlServerConfigurationSection : ConfigurationSection
+    {
+        private static MSSqlServerConfigurationSection settings =
+            ConfigurationManager.GetSection("MSSqlServerSettings") as MSSqlServerConfigurationSection;
+
+        /// <summary>
+        /// Access to the settings stored in the config file
+        /// </summary>
+        public static MSSqlServerConfigurationSection Settings
+        {
+            get
+            {
+                return settings;
+            }
+        }
+
+        /// <summary>
+        /// Columns in the database to write data into
+        /// </summary>
+        [ConfigurationProperty("Columns", IsDefaultCollection = false)]
+        [ConfigurationCollection(typeof(ColumnCollection),
+            AddItemName = "add",
+            ClearItemsName = "clear",
+            RemoveItemName = "remove")]
+        public ColumnCollection Columns
+        {
+            get
+            {
+                return (ColumnCollection)base["Columns"];
+            }
+        }
+    }
+}
