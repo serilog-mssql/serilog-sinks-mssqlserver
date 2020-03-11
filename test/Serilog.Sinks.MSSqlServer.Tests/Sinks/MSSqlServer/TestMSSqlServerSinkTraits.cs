eebcf1d4191ca41ebda3dc2af38ead8ae9de758b
Kadluba Christian <christian.kadluba.an@at.zurich.com>
2020-03-11 02:05:02 +0100

    Added unit tests.

--- test/Serilog.Sinks.MSSqlServer.Tests/Sinks/MSSqlServer/TestMSSqlServerSinkTraits.cs
+++ test/Serilog.Sinks.MSSqlServer.Tests/Sinks/MSSqlServer/TestMSSqlServerSinkTraits.cs
@@ -1,9 +1,12 @@
-﻿using Serilog.Events;
+﻿using Moq;
+using Serilog.Events;
+using Serilog.Formatting;
 using Serilog.Parsing;
 using System;
 using System.Collections.Generic;
 using System.Data;
 using System.Globalization;
+using System.IO;
 using System.Linq;
 using Xunit;
 
@@ -99,10 +102,52 @@ namespace Serilog.Sinks.MSSqlServer.Tests.Sinks.MSSqlServer
             Assert.Equal(new TimeSpan(0), timeStampColumnOffset.Offset);
         }
 
-        private void SetupTest(Serilog.Sinks.MSSqlServer.ColumnOptions options, DateTimeOffset testDateTimeOffset)
+        [Fact]
+        public void GetColumnsAndValuesWhenCalledWithCustomFormatterRendersLogEventPropertyUsingCustomFormatter()
+        {
+            // arrange
+            const string testLogEventContent = "Content of LogEvent";
+            var options = new Serilog.Sinks.MSSqlServer.ColumnOptions();
+            options.Store.Add(StandardColumn.LogEvent);
+            var logEventFormatterMock = new Mock<ITextFormatter>();
+            logEventFormatterMock.Setup(f => f.Format(It.IsAny<LogEvent>(), It.IsAny<TextWriter>()))
+                .Callback<LogEvent, TextWriter>((e, w) => w.Write(testLogEventContent));
+            SetupTest(options, DateTimeOffset.UtcNow, logEventFormatterMock.Object);
+
+            // act
+            var columns = traits.GetColumnsAndValues(logEvent);
+
+            // assert
+            var logEventColumn = columns.Single(c => c.Key == options.LogEvent.ColumnName);
+            Assert.Equal(testLogEventContent, logEventColumn.Value);
+        }
+
+        [Fact]
+        public void GetColumnsAndValuesWhenCalledWithoutFormatterRendersLogEventPropertyUsingInternalJsonFormatter()
+        {
+            // arrange
+            const string expectedLogEventContent =
+                "{\"TimeStamp\":\"2020-01-01T09:00:00.0000000\",\"Level\":\"Information\",\"Message\":\"\",\"MessageTemplate\":\"\"}";
+            var options = new Serilog.Sinks.MSSqlServer.ColumnOptions();
+            options.Store.Add(StandardColumn.LogEvent);
+            var testDateTimeOffset = new DateTimeOffset(2020, 1, 1, 9, 0, 0, TimeSpan.Zero);
+            SetupTest(options, testDateTimeOffset, null);
+
+            // act
+            var columns = traits.GetColumnsAndValues(logEvent);
+
+            // assert
+            var logEventColumn = columns.Single(c => c.Key == options.LogEvent.ColumnName);
+            Assert.Equal(expectedLogEventContent, logEventColumn.Value);
+        }
+
+        private void SetupTest(
+            Serilog.Sinks.MSSqlServer.ColumnOptions options,
+            DateTimeOffset testDateTimeOffset,
+            ITextFormatter logEventFormatter = null)
         {
             this.traits = new MSSqlServerSinkTraits("connectionString", "tableName", "schemaName", 
-                options, CultureInfo.InvariantCulture, false, null);
+                options, CultureInfo.InvariantCulture, false, logEventFormatter);
             this.logEvent = new LogEvent(testDateTimeOffset, LogEventLevel.Information, null,
                 new MessageTemplate(new List<MessageTemplateToken>()), new List<LogEventProperty>());
         }
