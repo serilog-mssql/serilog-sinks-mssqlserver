76d44e6d746523a9068c0cf405d69990f68a7ed5
Kadluba Christian <christian.kadluba.an@at.zurich.com>
2020-03-11 09:55:08 +0100

    Added a small sample app that demonstrates using MSSqlServerSink with a custom LogEvent column formatter.

---
+++ sample/CustomLogEventFormatterDemo/FlatLogEventFormatter.cs
@@ -0,0 +1,19 @@
+﻿using Serilog.Events;
+using Serilog.Formatting;
+using System.IO;
+using System.Linq;
+
+namespace CustomLogEventFormatterDemo
+{
+    public class FlatLogEventFormatter : ITextFormatter
+    {
+        public void Format(LogEvent logEvent, TextWriter output)
+        {
+            logEvent.Properties.ToList().ForEach(e =>
+                {
+                    output.Write($"{e.Key}={e.Value} ");
+                });
+            output.WriteLine();
+        }
+    }
+}
