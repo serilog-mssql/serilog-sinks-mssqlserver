5a6dad95453e2bf22181d0426cbb5d34da33b719
Christian Kadluba <10721825+ckadluba@users.noreply.github.com>
2020-04-11 21:44:19 +0200

    Added most unit tests for MicrosogtExtensionsConfigurationColumnOptionsProvider.

--- src/Serilog.Sinks.MSSqlServer/Configuration/Implementations/Microsoft.Extensions.Configuration/MicrosoftExtensionsColumnOptionsProvider.cs
+++ src/Serilog.Sinks.MSSqlServer/Configuration/Implementations/Microsoft.Extensions.Configuration/MicrosoftExtensionsColumnOptionsProvider.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using Microsoft.Extensions.Configuration;
 
 namespace Serilog.Sinks.MSSqlServer.Configuration
