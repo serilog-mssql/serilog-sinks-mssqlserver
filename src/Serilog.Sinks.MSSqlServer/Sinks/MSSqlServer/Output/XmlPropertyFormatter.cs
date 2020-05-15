f8d11903bdfbcb744fd7115628ff7da1cf1c6ed5
Christian Kadluba <10721825+ckadluba@users.noreply.github.com>
2020-05-15 21:27:23 +0200

    Restructured XmlPropertyFormatter into smaller private methods and added some unit tests for it.

--- src/Serilog.Sinks.MSSqlServer/Sinks/MSSqlServer/Output/XmlPropertyFormatter.cs
+++ src/Serilog.Sinks.MSSqlServer/Sinks/MSSqlServer/Output/XmlPropertyFormatter.cs
@@ -1,17 +1,3 @@
-// Copyright 2020 Serilog Contributors
-// 
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-// 
-//     http://www.apache.org/licenses/LICENSE-2.0
-// 
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
 using System.Globalization;
 using System.Linq;
 using System.Text;
@@ -30,140 +16,23 @@ namespace Serilog.Sinks.MSSqlServer.Output
         public string Simplify(LogEventPropertyValue value, ColumnOptions.PropertiesColumnOptions options)
         {
             if (value is ScalarValue scalar)
+            {
                 return SimplifyScalar(scalar.Value);
+            }
 
             if (value is DictionaryValue dict)
             {
-                var sb = new StringBuilder();
-
-                var isEmpty = true;
-
-                foreach (var element in dict.Elements)
-                {
-                    var itemValue = Simplify(element.Value, options);
-                    if (options.OmitElementIfEmpty && string.IsNullOrEmpty(itemValue))
-                    {
-                        continue;
-                    }
-
-                    if (isEmpty)
-                    {
-                        isEmpty = false;
-                        if (!options.OmitDictionaryContainerElement)
-                        {
-                            sb.AppendFormat(CultureInfo.InvariantCulture, "<{0}>", options.DictionaryElementName);
-                        }
-                    }
-
-                    var key = SimplifyScalar(element.Key.Value);
-                    if (options.UsePropertyKeyAsElementName)
-                    {
-                        sb.AppendFormat(CultureInfo.InvariantCulture, "<{0}>{1}</{0}>", GetValidElementName(key), itemValue);
-                    }
-                    else
-                    {
-                        sb.AppendFormat(CultureInfo.InvariantCulture, "<{0} key='{1}'>{2}</{0}>", options.ItemElementName, key, itemValue);
-                    }
-                }
-
-                if (!isEmpty && !options.OmitDictionaryContainerElement)
-                {
-                    sb.AppendFormat(CultureInfo.InvariantCulture, "</{0}>", options.DictionaryElementName);
-                }
-
-                return sb.ToString();
+                return SimplifyDictionary(options, dict);
             }
 
             if (value is SequenceValue seq)
             {
-                var sb = new StringBuilder();
-
-                var isEmpty = true;
-
-                foreach (var element in seq.Elements)
-                {
-                    var itemValue = Simplify(element, options);
-                    if (options.OmitElementIfEmpty && string.IsNullOrEmpty(itemValue))
-                    {
-                        continue;
-                    }
-
-                    if (isEmpty)
-                    {
-                        isEmpty = false;
-                        if (!options.OmitSequenceContainerElement)
-                        {
-                            sb.AppendFormat(CultureInfo.InvariantCulture, "<{0}>", options.SequenceElementName);
-                        }
-                    }
-
-                    sb.AppendFormat(CultureInfo.InvariantCulture, "<{0}>{1}</{0}>", options.ItemElementName, itemValue);
-                }
-
-                if (!isEmpty && !options.OmitSequenceContainerElement)
-                {
-                    sb.AppendFormat(CultureInfo.InvariantCulture, "</{0}>", options.SequenceElementName);
-                }
-
-                return sb.ToString();
+                return SimplifySequence(options, seq);
             }
 
             if (value is StructureValue str)
             {
-                var props = str.Properties.ToDictionary(p => p.Name, p => Simplify(p.Value, options));
-
-                var sb = new StringBuilder();
-
-                var isEmpty = true;
-
-                foreach (var element in props)
-                {
-                    var itemValue = element.Value;
-                    if (options.OmitElementIfEmpty && string.IsNullOrEmpty(itemValue))
-                    {
-                        continue;
-                    }
-
-                    if (isEmpty)
-                    {
-                        isEmpty = false;
-                        if (!options.OmitStructureContainerElement)
-                        {
-                            if (options.UsePropertyKeyAsElementName)
-                            {
-                                sb.AppendFormat(CultureInfo.InvariantCulture, "<{0}>", GetValidElementName(str.TypeTag));
-                            }
-                            else
-                            {
-                                sb.AppendFormat(CultureInfo.InvariantCulture, "<{0} type='{1}'>", options.StructureElementName, str.TypeTag);
-                            }
-                        }
-                    }
-
-                    if (options.UsePropertyKeyAsElementName)
-                    {
-                        sb.AppendFormat(CultureInfo.InvariantCulture, "<{0}>{1}</{0}>", GetValidElementName(element.Key), itemValue);
-                    }
-                    else
-                    {
-                        sb.AppendFormat(CultureInfo.InvariantCulture, "<{0} key='{1}'>{2}</{0}>", options.PropertyElementName,
-                            element.Key, itemValue);
-                    }
-                }
-
-                if (!isEmpty && !options.OmitStructureContainerElement)
-                {
-                    if (options.UsePropertyKeyAsElementName)
-                    {
-                        sb.AppendFormat(CultureInfo.InvariantCulture, "</{0}>", GetValidElementName(str.TypeTag));
-                    }
-                    else
-                    {
-                        sb.AppendFormat(CultureInfo.InvariantCulture, "</{0}>", options.StructureElementName);
-                    }
-                }
-
-                return sb.ToString();
+                return SimplifyStructure(options, str);
             }
 
             return null;
@@ -193,6 +62,140 @@ namespace Serilog.Sinks.MSSqlServer.Output
             if (value == null) return null;
 
             return new XText(_invalidXMLChars.Replace(value.ToString(), m => "\\u" + ((ushort)m.Value[0]).ToString("x4", CultureInfo.InvariantCulture))).ToString();
+        }
+
+        private string SimplifyDictionary(ColumnOptions.PropertiesColumnOptions options, DictionaryValue dict)
+        {
+            var sb = new StringBuilder();
+
+            var isEmpty = true;
+
+            foreach (var element in dict.Elements)
+            {
+                var itemValue = Simplify(element.Value, options);
+                if (options.OmitElementIfEmpty && string.IsNullOrEmpty(itemValue))
+                {
+                    continue;
+                }
+
+                if (isEmpty)
+                {
+                    isEmpty = false;
+                    if (!options.OmitDictionaryContainerElement)
+                    {
+                        sb.AppendFormat(CultureInfo.InvariantCulture, "<{0}>", options.DictionaryElementName);
+                    }
+                }
+
+                var key = SimplifyScalar(element.Key.Value);
+                if (options.UsePropertyKeyAsElementName)
+                {
+                    sb.AppendFormat(CultureInfo.InvariantCulture, "<{0}>{1}</{0}>", GetValidElementName(key), itemValue);
+                }
+                else
+                {
+                    sb.AppendFormat(CultureInfo.InvariantCulture, "<{0} key='{1}'>{2}</{0}>", options.ItemElementName, key, itemValue);
+                }
+            }
+
+            if (!isEmpty && !options.OmitDictionaryContainerElement)
+            {
+                sb.AppendFormat(CultureInfo.InvariantCulture, "</{0}>", options.DictionaryElementName);
+            }
+
+            return sb.ToString();
+        }
+
+        private string SimplifyStructure(ColumnOptions.PropertiesColumnOptions options, StructureValue str)
+        {
+            var props = str.Properties.ToDictionary(p => p.Name, p => Simplify(p.Value, options));
+
+            var sb = new StringBuilder();
+
+            var isEmpty = true;
+
+            foreach (var element in props)
+            {
+                var itemValue = element.Value;
+                if (options.OmitElementIfEmpty && string.IsNullOrEmpty(itemValue))
+                {
+                    continue;
+                }
+
+                if (isEmpty)
+                {
+                    isEmpty = false;
+                    if (!options.OmitStructureContainerElement)
+                    {
+                        if (options.UsePropertyKeyAsElementName)
+                        {
+                            sb.AppendFormat(CultureInfo.InvariantCulture, "<{0}>", GetValidElementName(str.TypeTag));
+                        }
+                        else
+                        {
+                            sb.AppendFormat(CultureInfo.InvariantCulture, "<{0} type='{1}'>", options.StructureElementName, str.TypeTag);
+                        }
+                    }
+                }
+
+                if (options.UsePropertyKeyAsElementName)
+                {
+                    sb.AppendFormat(CultureInfo.InvariantCulture, "<{0}>{1}</{0}>", GetValidElementName(element.Key), itemValue);
+                }
+                else
+                {
+                    sb.AppendFormat(CultureInfo.InvariantCulture, "<{0} key='{1}'>{2}</{0}>", options.PropertyElementName,
+                        element.Key, itemValue);
+                }
+            }
+
+            if (!isEmpty && !options.OmitStructureContainerElement)
+            {
+                if (options.UsePropertyKeyAsElementName)
+                {
+                    sb.AppendFormat(CultureInfo.InvariantCulture, "</{0}>", GetValidElementName(str.TypeTag));
+                }
+                else
+                {
+                    sb.AppendFormat(CultureInfo.InvariantCulture, "</{0}>", options.StructureElementName);
+                }
+            }
+
+            return sb.ToString();
+        }
+
+        private string SimplifySequence(ColumnOptions.PropertiesColumnOptions options, SequenceValue seq)
+        {
+            var sb = new StringBuilder();
+
+            var isEmpty = true;
+
+            foreach (var element in seq.Elements)
+            {
+                var itemValue = Simplify(element, options);
+                if (options.OmitElementIfEmpty && string.IsNullOrEmpty(itemValue))
+                {
+                    continue;
+                }
+
+                if (isEmpty)
+                {
+                    isEmpty = false;
+                    if (!options.OmitSequenceContainerElement)
+                    {
+                        sb.AppendFormat(CultureInfo.InvariantCulture, "<{0}>", options.SequenceElementName);
+                    }
+                }
+
+                sb.AppendFormat(CultureInfo.InvariantCulture, "<{0}>{1}</{0}>", options.ItemElementName, itemValue);
+            }
+
+            if (!isEmpty && !options.OmitSequenceContainerElement)
+            {
+                sb.AppendFormat(CultureInfo.InvariantCulture, "</{0}>", options.SequenceElementName);
+            }
+
+            return sb.ToString();
         }
     }
 }
--- test/Serilog.Sinks.MSSqlServer.Tests/Sinks/MSSqlServer/Output/XmlPropertyFormatterTests.cs
+++ test/Serilog.Sinks.MSSqlServer.Tests/Sinks/MSSqlServer/Output/XmlPropertyFormatterTests.cs
@@ -1,4 +1,6 @@
-﻿using Serilog.Sinks.MSSqlServer.Output;
+﻿using System.Collections.Generic;
+using Serilog.Events;
+using Serilog.Sinks.MSSqlServer.Output;
 using Xunit;
 
 namespace Serilog.Sinks.MSSqlServer.Tests.Sinks.MSSqlServer.Output
@@ -6,53 +8,214 @@ namespace Serilog.Sinks.MSSqlServer.Tests.Sinks.MSSqlServer.Output
     public class XmlPropertyFormatterTests
     {
         [Fact]
-        public void GetValidElementNameReturnsValidNameOnNull()
+        public void SimplifyScalarReplacesXmlInvalidChars()
         {
+            // Arrange
+            var input = new ScalarValue("some<allowed>words&inbetween");
             var sut = new XmlPropertyFormatter();
 
+            // Act
+            var result = sut.Simplify(input, null);
+
+            // Assert
+            Assert.Equal("some&lt;allowed&gt;words&amp;inbetween", result);
+        }
+
+        [Fact]
+        public void SimplifyDictionaryRendersElements()
+        {
+            // Arrange
+            var input = new DictionaryValue(new List<KeyValuePair<ScalarValue, LogEventPropertyValue>>
+            {
+                new KeyValuePair<ScalarValue, LogEventPropertyValue>(new ScalarValue("Key1"), new ScalarValue("Value1")),
+                new KeyValuePair<ScalarValue, LogEventPropertyValue>(new ScalarValue("Key2"), new ScalarValue(2))
+            });
+            var options = new Serilog.Sinks.MSSqlServer.ColumnOptions.PropertiesColumnOptions();
+            var sut = new XmlPropertyFormatter();
+
+            // Act
+            var result = sut.Simplify(input, options);
+
+            // Assert
+            Assert.Equal("<dictionary><item key='Key1'>Value1</item><item key='Key2'>2</item></dictionary>", result);
+        }
+
+        [Fact]
+        public void SimplifyDictionaryHandlesOptionUsePropertyKeyAsElementName()
+        {
+            // Arrange
+            var input = new DictionaryValue(new List<KeyValuePair<ScalarValue, LogEventPropertyValue>>
+            {
+                new KeyValuePair<ScalarValue, LogEventPropertyValue>(new ScalarValue("Key1"), new ScalarValue("Value1")),
+                new KeyValuePair<ScalarValue, LogEventPropertyValue>(new ScalarValue("Key2"), new ScalarValue(2))
+            });
+            var options = new Serilog.Sinks.MSSqlServer.ColumnOptions.PropertiesColumnOptions { UsePropertyKeyAsElementName = true };
+            var sut = new XmlPropertyFormatter();
+
+            // Act
+            var result = sut.Simplify(input, options);
+
+            // Assert
+            Assert.Equal("<dictionary><Key1>Value1</Key1><Key2>2</Key2></dictionary>", result);
+        }
+
+        [Fact]
+        public void SimplifyDictionaryReplacesXmlInvalidCharsInValue()
+        {
+            // Arrange
+            var input = new DictionaryValue(new List<KeyValuePair<ScalarValue, LogEventPropertyValue>>
+            {
+                new KeyValuePair<ScalarValue, LogEventPropertyValue>(new ScalarValue("Key1"), new ScalarValue("some<allowed>words&inbetween"))
+            });
+            var options = new Serilog.Sinks.MSSqlServer.ColumnOptions.PropertiesColumnOptions();
+            var sut = new XmlPropertyFormatter();
+
+            // Act
+            var result = sut.Simplify(input, options);
+
+            // Assert
+            Assert.Equal("<dictionary><item key='Key1'>some&lt;allowed&gt;words&amp;inbetween</item></dictionary>", result);
+        }
+
+        [Fact]
+        public void SimplifyDictionaryReplacesXmlInvalidCharsInKey()
+        {
+            // Arrange
+            var input = new DictionaryValue(new List<KeyValuePair<ScalarValue, LogEventPropertyValue>>
+            {
+                new KeyValuePair<ScalarValue, LogEventPropertyValue>(new ScalarValue("some<allowed>words&inbetween"), new ScalarValue("Value1"))
+            });
+            var options = new Serilog.Sinks.MSSqlServer.ColumnOptions.PropertiesColumnOptions();
+            var sut = new XmlPropertyFormatter();
+
+            // Act
+            var result = sut.Simplify(input, options);
+
+            // Assert
+            Assert.Equal("<dictionary><item key='some&lt;allowed&gt;words&amp;inbetween'>Value1</item></dictionary>", result);
+        }
+
+        [Fact]
+        public void SimplifyDictionaryHandlesOptionOmitElementIfEmpty()
+        {
+            // Arrange
+            var input = new DictionaryValue(new List<KeyValuePair<ScalarValue, LogEventPropertyValue>>
+            {
+                new KeyValuePair<ScalarValue, LogEventPropertyValue>(new ScalarValue("Key1"), new ScalarValue(string.Empty)),
+                new KeyValuePair<ScalarValue, LogEventPropertyValue>(new ScalarValue("Key2"), new ScalarValue("Value2")),
+                new KeyValuePair<ScalarValue, LogEventPropertyValue>(new ScalarValue("Key3"), new ScalarValue(null))
+            });
+            var options = new Serilog.Sinks.MSSqlServer.ColumnOptions.PropertiesColumnOptions { OmitElementIfEmpty = true };
+            var sut = new XmlPropertyFormatter();
+
+            // Act
+            var result = sut.Simplify(input, options);
+
+            // Assert
+            Assert.Equal("<dictionary><item key='Key2'>Value2</item></dictionary>", result);
+        }
+
+        [Fact]
+        public void SimplifyDictionaryHandlesOptionOmitDictionaryContainerElement()
+        {
+            // Arrange
+            var input = new DictionaryValue(new List<KeyValuePair<ScalarValue, LogEventPropertyValue>>
+            {
+                new KeyValuePair<ScalarValue, LogEventPropertyValue>(new ScalarValue("Key1"), new ScalarValue("Value1")),
+                new KeyValuePair<ScalarValue, LogEventPropertyValue>(new ScalarValue("Key2"), new ScalarValue("Value2"))
+            });
+            var options = new Serilog.Sinks.MSSqlServer.ColumnOptions.PropertiesColumnOptions { OmitDictionaryContainerElement = true };
+            var sut = new XmlPropertyFormatter();
+
+            // Act
+            var result = sut.Simplify(input, options);
+
+            // Assert
+            Assert.Equal("<item key='Key1'>Value1</item><item key='Key2'>Value2</item>", result);
+        }
+
+        [Fact]
+        public void SimplifyDictionaryUsesCustomDictionaryElementName()
+        {
+            // Arrange
+            var input = new DictionaryValue(new List<KeyValuePair<ScalarValue, LogEventPropertyValue>>
+            {
+                new KeyValuePair<ScalarValue, LogEventPropertyValue>(new ScalarValue("Key1"), new ScalarValue("Value1")),
+                new KeyValuePair<ScalarValue, LogEventPropertyValue>(new ScalarValue("Key2"), new ScalarValue("Value2"))
+            });
+            var options = new Serilog.Sinks.MSSqlServer.ColumnOptions.PropertiesColumnOptions { DictionaryElementName = "list" };
+            var sut = new XmlPropertyFormatter();
+
+            // Act
+            var result = sut.Simplify(input, options);
+
+            // Assert
+            Assert.Equal("<list><item key='Key1'>Value1</item><item key='Key2'>Value2</item></list>", result);
+        }
+
+        [Fact]
+        public void GetValidElementNameReturnsValidNameOnNull()
+        {
+            // Arrange
+            var sut = new XmlPropertyFormatter();
+
+            // Act
             var result = sut.GetValidElementName(null);
 
+            // Assert
             Assert.Equal("x", result);
         }
 
         [Fact]
         public void GetValidElementNameReturnsValidNameOnEmpty()
         {
+            // Arrange
             var sut = new XmlPropertyFormatter();
 
+            // Act
             var result = sut.GetValidElementName(string.Empty);
 
+            // Assert
             Assert.Equal("x", result);
         }
 
         [Fact]
         public void GetValidElementNameTrimsWhitespace()
         {
+            // Arrange
             var sut = new XmlPropertyFormatter();
 
+            // Act
             var result = sut.GetValidElementName("  \tname   \t");
 
+            // Assert
             Assert.Equal("name", result);
         }
 
         [Fact]
         public void GetValidElementNamePrependsXIfNonLetterAtBegin()
         {
+            // Arrange
             var sut = new XmlPropertyFormatter();
 
+            // Act
             var result = sut.GetValidElementName("$Name");
 
+            // Assert
             Assert.Equal("x$Name", result);
         }
 
         [Fact]
         public void GetValidElementNamePrependsXIfXmlAtBeginCaseInsensitive()
         {
+            // Arrange
             var sut = new XmlPropertyFormatter();
 
+            // Act + assert
             var result = sut.GetValidElementName("xmlName");
             Assert.Equal("xxmlName", result);
 
+            // Act + assert
             var result2 = sut.GetValidElementName("XmlName");
             Assert.Equal("xXmlName", result2);
         }
@@ -60,11 +223,14 @@ namespace Serilog.Sinks.MSSqlServer.Tests.Sinks.MSSqlServer.Output
         [Fact]
         public void GetValidElementNameTrimsWhitespaceAndPrependsXIfInvalidBegin()
         {
+            // Arrange
             var sut = new XmlPropertyFormatter();
 
+            // Act + assert
             var result = sut.GetValidElementName(" xmlName ");
             Assert.Equal("xxmlName", result);
 
+            // Act + assert
             var result2 = sut.GetValidElementName(" 4Name ");
             Assert.Equal("x4Name", result2);
         }
@@ -72,10 +238,13 @@ namespace Serilog.Sinks.MSSqlServer.Tests.Sinks.MSSqlServer.Output
         [Fact]
         public void GetValidElementNameReplacesEnclosedWhitespaceWithUnderscore()
         {
+            // Arrange
             var sut = new XmlPropertyFormatter();
 
+            // Act
             var result = sut.GetValidElementName("Name\tWith   Space");
 
+            // Assert
             Assert.Equal("Name_With___Space", result);
         }
     }
