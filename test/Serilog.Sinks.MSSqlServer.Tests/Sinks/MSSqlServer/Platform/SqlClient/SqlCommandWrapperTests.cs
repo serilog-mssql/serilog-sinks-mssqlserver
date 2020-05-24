3fb046f534f10bbb1649824b1fa908dd27189292
Christian Kadluba <10721825+ckadluba@users.noreply.github.com>
2020-05-24 23:29:43 +0200

    Added unit tests for SqlClient wrapper classes.

---
+++ test/Serilog.Sinks.MSSqlServer.Tests/Sinks/MSSqlServer/Platform/SqlClient/SqlCommandWrapperTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+#if NET452
+using System.Data.SqlClient;
+#else
+using Microsoft.Data.SqlClient;
+#endif
+using Xunit;
+using Serilog.Sinks.MSSqlServer.Tests.TestUtils;
+using Serilog.Sinks.MSSqlServer.Platform.SqlClient;
+
+namespace Serilog.Sinks.MSSqlServer.Tests.Sinks.MSSqlServer.Platform.SqlClient
+{
+    [Trait(TestCategory.TraitName, TestCategory.Unit)]
+    public class SqlCommandWrapperTests
+    {
+        [Fact]
+        public void InitializeThrowsIfSqlCommandIsNull()
+        {
+            Assert.Throws<ArgumentNullException>(() => new SqlCommandWrapper(null));
+        }
+
+        [Fact]
+        public void AddParameterDoesNotThrow()
+        {
+            // Arrange
+            using (var sqlCommand = new SqlCommand())
+            {
+                using (var sut = new SqlCommandWrapper(sqlCommand))
+                {
+                    // Act (should not throw)
+                    sut.AddParameter("Parameter", "Value");
+                }
+            }
+        }
+    }
+}
