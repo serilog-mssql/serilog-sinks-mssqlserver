fff48d5828047ea91b0e86b323327dfa296ae326
Christian Kadluba <10721825+ckadluba@users.noreply.github.com>
2022-02-18 00:08:49 +0100

    GitHub actions (#387)

--- Build.ps1
+++ Build.ps1
@@ -1,3 +1,10 @@
+[CmdletBinding()]
+param (
+    [Parameter(Mandatory = $false)]
+    [System.Boolean]
+    $RunTests = $false
+)
+
 echo "build: Build started"
 
 Push-Location $PSScriptRoot
@@ -9,8 +16,8 @@ if(Test-Path .\artifacts) {
 
 & dotnet restore --no-cache
 
-$branch = @{ $true = $env:APPVEYOR_REPO_BRANCH; $false = $(git symbolic-ref --short -q HEAD) }[$env:APPVEYOR_REPO_BRANCH -ne $NULL];
-$revision = @{ $true = "{0:00000}" -f [convert]::ToInt32("0" + $env:APPVEYOR_BUILD_NUMBER, 10); $false = "local" }[$env:APPVEYOR_BUILD_NUMBER -ne $NULL];
+$branch = @{ $true = $env:GITHUB_REF_NAME; $false = $(git symbolic-ref --short -q HEAD) }[$env:GITHUB_REF_NAME -ne $NULL];
+$revision = @{ $true = "{0:00000}" -f [convert]::ToInt32("0" + $env:GITHUB_RUN_NUMBER, 10); $false = "local" }[$env:GITHUB_RUN_NUMBER -ne $NULL];
 $suffix = @{ $true = ""; $false = "$($branch.Substring(0, [math]::Min(10,$branch.Length)))-$revision"}[$branch -eq "master" -and $revision -ne "local"]
 
 echo "build: Version suffix is $suffix"
@@ -30,26 +37,28 @@ foreach ($src in ls src/*) {
     Pop-Location
 }
 
-foreach ($test in ls test/*.PerformanceTests) {
-    Push-Location $test
+if ($RunTests -eq $true) {
+    foreach ($test in ls test/*.PerformanceTests) {
+        Push-Location $test
 
-    echo "build: Building performance test project in $test"
+        echo "build: Building performance test project in $test"
 
-    & dotnet build -c Release
-    if($LASTEXITCODE -ne 0) { exit 2 }
+        & dotnet build -c Release
+        if($LASTEXITCODE -ne 0) { exit 2 }
 
-    Pop-Location
-}
+        Pop-Location
+    }
 
-foreach ($test in ls test/*.Tests) {
-    Push-Location $test
+    foreach ($test in ls test/*.Tests) {
+        Push-Location $test
 
-    echo "build: Testing project in $test"
+        echo "build: Testing project in $test"
 
-    & dotnet test -c Release
-    if($LASTEXITCODE -ne 0) { exit 3 }
+        & dotnet test -c Release
+        if($LASTEXITCODE -ne 0) { exit 3 }
 
-    Pop-Location
+        Pop-Location
+    }
 }
 
 Pop-Location
