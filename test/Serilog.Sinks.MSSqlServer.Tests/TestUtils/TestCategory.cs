46a8358dd269edf12f131df9f3773840da22a6a6
Christian Kadluba <10721825+ckadluba@users.noreply.github.com>
2022-03-04 18:54:36 +0100

    Execute test LogsAreNotAffectedByTransactionsByDefault in a separate run from all other tests to avoid side effects with other DB tests. Ugly but the only feasible way to get this stable.

--- test/Serilog.Sinks.MSSqlServer.Tests/TestUtils/TestCategory.cs
+++ test/Serilog.Sinks.MSSqlServer.Tests/TestUtils/TestCategory.cs
@@ -5,6 +5,7 @@
         public const string TraitName = "Category";
 
         public const string Integration = nameof(Integration);
+        public const string Isolated = nameof(Isolated);
         public const string Unit = nameof(Unit);
     }
 }
---
+++ test/Serilog.Sinks.MSSqlServer.Tests/TransactionTests.cs
@@ -0,0 +1,47 @@
+﻿using System.Transactions;
+using FluentAssertions;
+using Serilog.Sinks.MSSqlServer.Tests.TestUtils;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Serilog.Sinks.MSSqlServer.Tests
+{
+    [Trait(TestCategory.TraitName, TestCategory.Isolated)]
+    public class TransactionTests : DatabaseTestsBase
+    {
+        public TransactionTests(ITestOutputHelper output) : base(output)
+        {
+        }
+
+        [Fact]
+        public void LogsAreNotAffectedByTransactionsByDefault()
+        {
+            // Arrange
+            Log.Logger = new LoggerConfiguration()
+                .WriteTo.MSSqlServer
+                (
+                    connectionString: DatabaseFixture.LogEventsConnectionString,
+                    new MSSqlServerSinkOptions
+                    {
+                        TableName = DatabaseFixture.LogTableName,
+                        AutoCreateSqlTable = true
+                    }
+                )
+                .CreateLogger();
+
+            using (var scope = new TransactionScope(TransactionScopeAsyncFlowOption.Enabled))
+            {
+                // Act
+                Log.Logger.Information("Logging message");
+
+                // Flush message so it is written on foreground thread instead of timer
+                // So we can test if it is affected by transaction
+                Log.CloseAndFlush();
+            }
+
+            // Assert after rollback, the message should still be persisted
+            VerifyCustomQuery<LogEventColumn>($"SELECT Id from {DatabaseFixture.LogTableName}",
+                e => e.Should().HaveCount(1));
+        }
+    }
+}
