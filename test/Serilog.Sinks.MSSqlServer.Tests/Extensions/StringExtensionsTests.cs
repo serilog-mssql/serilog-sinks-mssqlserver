c98adeb6d65ea54afdc4edb19039dcb86d6aed33
studiopasokon <andy@studiopasokon.com>
2022-03-11 10:18:30 +0100

    Add (slightly) general Truncate extension method

---
+++ test/Serilog.Sinks.MSSqlServer.Tests/Extensions/StringExtensionsTests.cs
@@ -0,0 +1,91 @@
+using Serilog.Sinks.MSSqlServer.Extensions;
+using Serilog.Sinks.MSSqlServer.Tests.TestUtils;
+using Xunit;
+
+namespace Serilog.Sinks.MSSqlServer.Tests.Extensions
+{
+    [Trait(TestCategory.TraitName, TestCategory.Unit)]
+    public class StringExtensionsTests
+    {
+        [Fact]
+        public void ReturnNullWhenInputStringIsNull()
+        {
+            // Arrange
+            string inputMessage = null;
+
+            // Act
+            var nonTruncatedMessage = inputMessage.Truncate(5, "...");
+
+            // Assert
+            Assert.Null(nonTruncatedMessage);
+        }
+
+        [Fact]
+        public void ReturnEmptyWhenInputStringIsEmpty()
+        {
+            // Arrange
+            var inputMessage = "";
+
+            // Act
+            var nonTruncatedMessage = inputMessage.Truncate(5, "...");
+
+            // Assert
+            Assert.Equal(inputMessage, nonTruncatedMessage);
+        }
+
+        [Theory]
+        [InlineData(0)]
+        [InlineData(-1)]
+        [InlineData(-5)]
+        public void ReturnEmptyStringWhenRequestedMaxValueIsZeroOrSmaller(int maxValue)
+        {
+            // Arrange
+            var inputMessage = "A simple test message";
+
+            // Act
+            var nonTruncatedMessage = inputMessage.Truncate(maxValue, "...");
+
+            // Assert
+            Assert.Equal("", nonTruncatedMessage);
+        }
+
+        [Fact]
+        public void ReturnTruncatedStringWithSuffix()
+        {
+            // Arrange
+            var inputMessage = "A simple test message";
+
+            // Act
+            var truncatedMessage = inputMessage.Truncate(15, "...");
+
+            // Assert
+            Assert.Equal("A simple tes...", truncatedMessage);
+        }
+
+        [Fact]
+        public void ReturnTruncatedStringWithEmptySuffix()
+        {
+            // Arrange
+            var inputMessage = "A simple test message";
+
+            // Act
+            var truncatedMessage = inputMessage.Truncate(15, "");
+
+            // Assert
+            Assert.Equal("A simple test m", truncatedMessage);
+        }
+
+        [Fact]
+        public void ReturnTruncatedStringWithNullSuffix()
+        {
+            // Arrange
+            var inputMessage = "A simple test message";
+
+            // Act
+            var truncatedMessage = inputMessage.Truncate(15, null);
+
+            // Assert
+            Assert.Equal("A simple test m", truncatedMessage);
+        }
+    }
+}
