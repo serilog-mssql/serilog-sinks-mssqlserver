bcb4f6b7d08b17376085a2afac326fde004ee7ca
studiopasokon <andy@studiopasokon.com>
2022-03-11 10:18:30 +0100

    Add (slightly) general Truncate extension method

---
+++ src/Serilog.Sinks.MSSqlServer/Extensions/StringExtensions.cs
@@ -0,0 +1,15 @@
+namespace Serilog.Sinks.MSSqlServer.Extensions
+{
+    internal static class StringExtensions
+    {
+        public static string Truncate(this string value, int maxLength, string suffix)
+        {
+            if (value == null) return null;
+            var suffixLength = suffix?.Length ?? 0;
+            if (maxLength <= suffixLength) return string.Empty;
+
+            var correctedMaxLength = maxLength - suffixLength;
+            return value.Length <= maxLength ? value : $"{value.Substring(0, correctedMaxLength)}{suffix}";
+        }
+    }
+}
