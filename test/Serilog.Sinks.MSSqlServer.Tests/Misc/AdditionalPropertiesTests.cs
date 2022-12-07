bea18919cea37707be2d233c6e06ce807842bce3
Christian Kadluba <10721825+ckadluba@users.noreply.github.com>
2022-12-07 16:31:41 +0100

    Added an integration test.

--- test/Serilog.Sinks.MSSqlServer.Tests/Misc/AdditionalPropertiesTests.cs
+++ test/Serilog.Sinks.MSSqlServer.Tests/Misc/AdditionalPropertiesTests.cs
@@ -123,5 +123,69 @@ namespace Serilog.Sinks.MSSqlServer.Tests.Misc
             VerifyStringColumnWritten(additionalColumn1Name, property1Value);
             VerifyIntegerColumnWritten(additionalColumn2Name, property2Value);
         }
+
+        [Fact]
+        public void WritesLogEventWithColumnsFromHierarchicalNamedProperties()
+        {
+            // Arrange
+            const string additionalColumn1Name = "AdditionalColumn1";
+            const string additionalProperty1Name = "AdditionalProperty1.SubProperty1";
+            const string additionalColumn2Name = "AdditionalColumn2";
+            const string additionalProperty2Name = "AdditionalProperty2.SubProperty2.SubSubProperty1";
+            var columnOptions = new MSSqlServer.ColumnOptions
+            {
+                AdditionalColumns = new List<SqlColumn>
+                {
+                    new SqlColumn
+                    {
+                        ColumnName = additionalColumn1Name,
+                        PropertyName = additionalProperty1Name,
+                        DataType = SqlDbType.NVarChar,
+                        AllowNull = true,
+                        DataLength = 100
+                    },
+                    new SqlColumn
+                    {
+                        ColumnName = additionalColumn2Name,
+                        PropertyName = additionalProperty2Name,
+                        DataType = SqlDbType.Int,
+                        AllowNull = true
+                    }
+                }
+            };
+            var property1Value = "PropertyValue1";
+            var property2Value = 2;
+
+            // Act
+            Log.Logger = new LoggerConfiguration()
+                .WriteTo.MSSqlServer(
+                    DatabaseFixture.LogEventsConnectionString,
+                    sinkOptions: new MSSqlServerSinkOptions
+                    {
+                        TableName = DatabaseFixture.LogTableName,
+                        AutoCreateSqlTable = true
+                    },
+                    columnOptions: columnOptions,
+                    formatProvider: CultureInfo.InvariantCulture)
+                .CreateLogger();
+            Log.Information("Hello {@AdditionalProperty1} from thread {@AdditionalProperty2}",
+                new StructuredType
+                {
+                    SubProperty1 = property1Value
+                },
+                new StructuredType
+                {
+                    SubProperty2 = new StructuredSubType
+                    {
+                        SubSubProperty1 = property2Value
+                    }
+                });
+            Log.CloseAndFlush();
+
+            // Assert
+            VerifyDatabaseColumnsWereCreated(columnOptions.AdditionalColumns);
+            VerifyStringColumnWritten(additionalColumn1Name, property1Value);
+            VerifyIntegerColumnWritten(additionalColumn2Name, property2Value);
+        }
     }
 }
