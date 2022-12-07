a131a459f04b0794c84ab7dc7243632083d345bc
Christian Kadluba <10721825+ckadluba@users.noreply.github.com>
2022-12-07 16:25:52 +0100

    Hierarchical property name support in SqlColumn class

--- test/Serilog.Sinks.MSSqlServer.Tests/Sinks/MSSqlServer/SqlServerColumnTests.cs
+++ test/Serilog.Sinks.MSSqlServer.Tests/Sinks/MSSqlServer/SqlServerColumnTests.cs
@@ -1,6 +1,7 @@
 ﻿using System.Data;
 using Serilog.Sinks.MSSqlServer.Tests.TestUtils;
 using Xunit;
+using static System.FormattableString;
 
 namespace Serilog.Sinks.MSSqlServer.Tests
 {
@@ -32,6 +33,33 @@ namespace Serilog.Sinks.MSSqlServer.Tests
 
             // Assert
             Assert.Equal(propertyName, sut.PropertyName);
+            Assert.Equal(1, sut.PropertyNameHierarchy.Count);
+            Assert.Equal(propertyName, sut.PropertyNameHierarchy[0]);
+            Assert.False(sut.HasHierarchicalPropertyName);
+        }
+
+        [Fact]
+        public void StoresHierachicalPropertyName()
+        {
+            // Arrange
+            const string propertyName1 = "TestPropertyName";
+            const string propertyName2 = "SubPropertyName";
+            const string propertyName3 = "SubSubPropertyName";
+            var propertyName = Invariant($"{propertyName1}.{propertyName2}.{propertyName3}");
+
+            // Act
+            var sut = new SqlColumn("TestColumnName", SqlDbType.Int)
+            {
+                PropertyName = propertyName
+            };
+
+            // Assert
+            Assert.Equal(propertyName, sut.PropertyName);
+            Assert.Equal(3, sut.PropertyNameHierarchy.Count);
+            Assert.Equal(propertyName1, sut.PropertyNameHierarchy[0]);
+            Assert.Equal(propertyName2, sut.PropertyNameHierarchy[1]);
+            Assert.Equal(propertyName3, sut.PropertyNameHierarchy[2]);
+            Assert.True(sut.HasHierarchicalPropertyName);
         }
     }
 }
