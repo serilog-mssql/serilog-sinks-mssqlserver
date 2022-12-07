951273113b5db44ee0eea73d168747ef6ca043f5
Christian Kadluba <10721825+ckadluba@users.noreply.github.com>
2022-12-07 16:31:41 +0100

    Added an integration test.

---
+++ test/Serilog.Sinks.MSSqlServer.Tests/Misc/StructuredType.cs
@@ -0,0 +1,8 @@
+﻿namespace Serilog.Sinks.MSSqlServer.Tests.Misc
+{
+    internal class StructuredType
+    {
+        public string SubProperty1 { get; set; }
+        public StructuredSubType SubProperty2 { get; set; }
+    }
+}
