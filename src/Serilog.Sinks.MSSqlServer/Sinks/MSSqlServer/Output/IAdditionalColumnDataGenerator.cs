0660ae716271c9fc8f9d5173d6d5d7b7e5f3dae8
Christian Kadluba <10721825+ckadluba@users.noreply.github.com>
2022-12-07 16:30:30 +0100

    Resolve hierarchical property expressions for additional columns
    * Refactored processing of AdditionalProperties: do not iterate LogEvent properties but iterate columns (more efficient and allows binding one property to multiple columns).
    * Refactored PropertiesColumnDataGenerator into three new classes: AdditionalColumnDataGenerator, ColumnSimplePropertyDataGenerator and ColumnHierarchicalPropertyDataGenerator.
    * Restructured, repaired and added unit tests.

---
+++ src/Serilog.Sinks.MSSqlServer/Sinks/MSSqlServer/Output/IAdditionalColumnDataGenerator.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using Serilog.Events;
+
+namespace Serilog.Sinks.MSSqlServer.Output
+{
+    internal interface IAdditionalColumnDataGenerator
+    {
+        KeyValuePair<string, object> GetAdditionalColumnNameAndValue(SqlColumn additionalColumn, IReadOnlyDictionary<string, LogEventPropertyValue> properties);
+    }
+}
---
+++ test/Serilog.Sinks.MSSqlServer.Tests/Sinks/MSSqlServer/Output/AdditionalColumnDataGeneratorTests.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using Moq;
+using Serilog.Events;
+using Serilog.Sinks.MSSqlServer.Output;
+using Serilog.Sinks.MSSqlServer.Sinks.MSSqlServer.Output;
+using Serilog.Sinks.MSSqlServer.Tests.TestUtils;
+using Xunit;
+
+namespace Serilog.Sinks.MSSqlServer.Tests.Output
+{
+    [Trait(TestCategory.TraitName, TestCategory.Unit)]
+    public class AdditionalColumnDataGeneratorTests
+    {
+        private readonly Dictionary<string, LogEventPropertyValue> _properties;
+        private Mock<IColumnSimplePropertyValueResolver> _columnSimplePropertyValueResolver;
+        private Mock<IColumnHierarchicalPropertyValueResolver> _columnHierarchicalPropertyValueResolver;
+        private AdditionalColumnDataGenerator _sut;
+
+        public AdditionalColumnDataGeneratorTests()
+        {
+            _properties = new Dictionary<string, LogEventPropertyValue>();
+            _columnSimplePropertyValueResolver = new Mock<IColumnSimplePropertyValueResolver>();
+            _columnHierarchicalPropertyValueResolver = new Mock<IColumnHierarchicalPropertyValueResolver>();
+            _sut = new AdditionalColumnDataGenerator(_columnSimplePropertyValueResolver.Object,
+                _columnHierarchicalPropertyValueResolver.Object);
+        }
+
+        [Fact]
+        public void GetAdditionalColumnNameAndValueReturnsCorrectSimplePropertyValue()
+        {
+            // Arrange
+            const string columnName = "AdditionalProperty1";
+            const string propertyValue = "Additonal Property Value";
+            var additionalColumn = new SqlColumn(columnName, SqlDbType.NVarChar);
+            var properties = new Dictionary<string, LogEventPropertyValue>();
+            _columnSimplePropertyValueResolver.Setup(r => r.GetPropertyValueForColumn(
+                It.IsAny<SqlColumn>(), It.IsAny<IReadOnlyDictionary<string, LogEventPropertyValue>>()))
+                .Returns(new KeyValuePair<string, LogEventPropertyValue>(columnName, new ScalarValue(propertyValue)));
+
+            // Act
+            var result = _sut.GetAdditionalColumnNameAndValue(additionalColumn, properties);
+
+            // Assert
+            _columnSimplePropertyValueResolver.Verify(r => r.GetPropertyValueForColumn(additionalColumn, properties), Times.Once);
+            Assert.Equal(columnName, result.Key);
+            Assert.Equal(propertyValue, result.Value);
+        }
+
+        [Fact]
+        public void GetAdditionalColumnNameAndValueReturnsNullForNotFoundSimpleProperty()
+        {
+            // Arrange
+            const string columnName = "AdditionalProperty1";
+            var additionalColumn = new SqlColumn(columnName, SqlDbType.NVarChar);
+            var properties = new Dictionary<string, LogEventPropertyValue>();
+            _columnSimplePropertyValueResolver.Setup(r => r.GetPropertyValueForColumn(
+                It.IsAny<SqlColumn>(), It.IsAny<IReadOnlyDictionary<string, LogEventPropertyValue>>()))
+                .Returns(default(KeyValuePair<string, LogEventPropertyValue>));
+
+            // Act
+            var result = _sut.GetAdditionalColumnNameAndValue(additionalColumn, properties);
+
+            // Assert
+            _columnSimplePropertyValueResolver.Verify(r => r.GetPropertyValueForColumn(additionalColumn, properties), Times.Once);
+            Assert.Equal(columnName, result.Key);
+            Assert.Equal(DBNull.Value, result.Value);
+        }
+
+        [Fact]
+        public void GetAdditionalColumnNameAndValueReturnsCorrectHierachicalPropertyValue()
+        {
+            // Arrange
+            const string columnName = "SubSubProperty1";
+            const string propertyValue = "Additonal Property Value";
+            var additionalColumn = new SqlColumn(columnName, SqlDbType.NVarChar)
+            {
+                PropertyName = "AdditionalProperty1.SubProperty2.SubSubProperty1"
+            };
+            var properties = new Dictionary<string, LogEventPropertyValue>();
+            _columnHierarchicalPropertyValueResolver.Setup(r => r.GetPropertyValueForColumn(
+                It.IsAny<SqlColumn>(), It.IsAny<IReadOnlyDictionary<string, LogEventPropertyValue>>()))
+                .Returns(new KeyValuePair<string, LogEventPropertyValue>(columnName, new ScalarValue(propertyValue)));
+
+            // Act
+            var result = _sut.GetAdditionalColumnNameAndValue(additionalColumn, properties);
+
+            // Assert
+            _columnHierarchicalPropertyValueResolver.Verify(r => r.GetPropertyValueForColumn(additionalColumn, properties), Times.Once);
+            Assert.Equal(columnName, result.Key);
+            Assert.Equal(propertyValue, result.Value);
+        }
+
+        [Fact]
+        public void GetAdditionalColumnNameAndValueReturnsNullForNotFoundHierachicalProperty()
+        {
+            // Arrange
+            const string columnName = "SubSubProperty1";
+            var additionalColumn = new SqlColumn(columnName, SqlDbType.NVarChar)
+            {
+                PropertyName = "AdditionalProperty1.SubProperty2.SubSubProperty1"
+            };
+            var properties = new Dictionary<string, LogEventPropertyValue>();
+            _columnHierarchicalPropertyValueResolver.Setup(r => r.GetPropertyValueForColumn(
+                It.IsAny<SqlColumn>(), It.IsAny<IReadOnlyDictionary<string, LogEventPropertyValue>>()))
+                .Returns(default(KeyValuePair<string, LogEventPropertyValue>));
+
+            // Act
+            var result = _sut.GetAdditionalColumnNameAndValue(additionalColumn, properties);
+
+            // Assert
+            _columnHierarchicalPropertyValueResolver.Verify(r => r.GetPropertyValueForColumn(additionalColumn, properties), Times.Once);
+            Assert.Equal(columnName, result.Key);
+            Assert.Equal(DBNull.Value, result.Value);
+        }
+
+        [Fact]
+        public void GetAdditionalColumnNameAndValueConvertsValueTypeToStringIfConversionToColumnTypeFails()
+        {
+            // Arrange
+            const string columnName = "AdditionalProperty1";
+            const int propertyValue = 1;
+            var additionalColumn = new SqlColumn(columnName, SqlDbType.DateTimeOffset);
+            var properties = new Dictionary<string, LogEventPropertyValue>();
+            _columnSimplePropertyValueResolver.Setup(r => r.GetPropertyValueForColumn(
+                It.IsAny<SqlColumn>(), It.IsAny<IReadOnlyDictionary<string, LogEventPropertyValue>>()))
+                .Returns(new KeyValuePair<string, LogEventPropertyValue>(columnName, new ScalarValue(propertyValue)));
+
+            // Act
+            var result = _sut.GetAdditionalColumnNameAndValue(additionalColumn, properties);
+
+            // Assert
+            _columnSimplePropertyValueResolver.Verify(r => r.GetPropertyValueForColumn(additionalColumn, properties), Times.Once);
+            Assert.Equal(columnName, result.Key);
+            Assert.IsType<string>(result.Value);
+            Assert.Equal("1", result.Value);  // Cannot convert int to SqlDbType.DateTimeOffset so returns string
+        }
+
+        [Fact]
+        public void GetAdditionalColumnNameAndValueConvertsUniqueIdentifier()
+        {
+            // Arrange
+            const string columnName = "AdditionalProperty1";
+            const string propertyValue = "7526f485-ec2d-4ec8-bd73-12a7d1c49a5d";
+            var additionalColumn = new SqlColumn(columnName, SqlDbType.UniqueIdentifier);
+            var properties = new Dictionary<string, LogEventPropertyValue>();
+            _columnSimplePropertyValueResolver.Setup(r => r.GetPropertyValueForColumn(
+                It.IsAny<SqlColumn>(), It.IsAny<IReadOnlyDictionary<string, LogEventPropertyValue>>()))
+                .Returns(new KeyValuePair<string, LogEventPropertyValue>(columnName, new ScalarValue(propertyValue)));
+
+            // Act
+            var result = _sut.GetAdditionalColumnNameAndValue(additionalColumn, properties);
+
+            // Assert
+            _columnSimplePropertyValueResolver.Verify(r => r.GetPropertyValueForColumn(additionalColumn, properties), Times.Once);
+            Assert.Equal(columnName, result.Key);
+            Assert.IsType<Guid>(result.Value);
+            var expectedResult = Guid.Parse(propertyValue);
+            Assert.Equal(expectedResult, result.Value);
+        }
+    }
+}
