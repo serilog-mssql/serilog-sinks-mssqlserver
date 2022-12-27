36c4fbfb0364cbc9fe68cb61d91b30a24c5bdcca
Christian Kadluba <10721825+ckadluba@users.noreply.github.com>
2022-12-28 00:52:58 +0100

    Rethrow general exception in SqlBulkBatchWriter.
    Allows to remove a code analysis and does not change behaviour of the sink since caller (PeriodicBatchingSink) catches all exceptions anyway.
    
    Related to #432.

--- src/Serilog.Sinks.MSSqlServer/Sinks/MSSqlServer/Platform/SqlBulkBatchWriter.cs
+++ src/Serilog.Sinks.MSSqlServer/Sinks/MSSqlServer/Platform/SqlBulkBatchWriter.cs
@@ -58,6 +58,7 @@ namespace Serilog.Sinks.MSSqlServer.Platform
             {
                 SelfLog.WriteLine("Unable to write batch of {0} log events to the database due to following error: {1}",
                     events.Count(), ex);
+                throw;
             }
             finally
             {
