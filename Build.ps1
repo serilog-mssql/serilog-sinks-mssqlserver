0116279e9f748bbc495f112bae07461bf2babda3
Christian Kadluba <10721825+ckadluba@users.noreply.github.com>
2024-10-28 14:15:46 +0100

    Improved build and perftest scripts
    * Do not use loops since we only have one project for main sink, one for tests and one for perftests.
    * Use try-finally blocks to pop back to caller's initial dir on errors.
    * Fail the build if there were nuget restore errors (e.g. due to known vulns).
    * build also sample projects on PR (skip them in release.yml and perftests.yml).

--- Build.ps1
+++ Build.ps1
@@ -2,61 +2,135 @@
 param (
     [Parameter(Mandatory = $false)]
     [Switch]
-    $SkipTests
+    $SkipTests,
+
+    [Parameter(Mandatory = $false)]
+    [Switch]
+    $SkipPerfTests,
+
+    [Parameter(Mandatory = $false)]
+    [Switch]
+    $SkipSamples
 )
 
 echo "build: Build started"
 
-Push-Location "$PSScriptRoot"
+try
+{
+    Push-Location "$PSScriptRoot"
 
-if (Test-Path .\artifacts) {
-    echo "build: Cleaning .\artifacts"
-    Remove-Item .\artifacts -Force -Recurse
-}
-
-& dotnet restore --no-cache
-
-$branch = @{ $true = $env:GITHUB_REF_NAME; $false = $(git symbolic-ref --short -q HEAD) }[$env:GITHUB_REF_NAME -ne $NULL]
-$revision = @{ $true = "{0:00000}" -f [convert]::ToInt32("0" + $env:GITHUB_RUN_NUMBER, 10); $false = "local" }[$env:GITHUB_RUN_NUMBER -ne $NULL]
-$suffix = @{ $true = ""; $false = "$($branch.Substring(0, [math]::Min(10, $branch.Length)))-$revision" }[$branch -ne "dev" -and $revision -ne "local"]
-
-echo "build: Version suffix is $suffix"
-
-foreach ($src in Get-ChildItem "$PSScriptRoot/src" -Directory) {
-    Push-Location $src.FullName
-
-    echo "build: Packaging project in $($src.FullName)"
-
-    if ($suffix) {
-        & dotnet pack -c Release -o ..\..\artifacts --version-suffix=$suffix
-    } else {
-        & dotnet pack -c Release -o ..\..\artifacts
+    if (Test-Path .\artifacts)
+    {
+        echo "build: Cleaning .\artifacts"
+        Remove-Item .\artifacts -Force -Recurse
     }
-    if ($LASTEXITCODE -ne 0) { exit 1 }
 
-    Pop-Location
-}
+    echo "build: Restoring packages for solution"
+    & dotnet restore --no-cache
+    if ($LASTEXITCODE -ne 0)
+    {
+        echo "Error returned by dotnet restore. Aborting build."
+        exit 1
+    }
 
-if ($SkipTests -eq $false) {
-    foreach ($test in Get-ChildItem "$PSScriptRoot/test" -Filter "*.Tests" -Directory) {
-        Push-Location $test.FullName
+    $branch = @{ $true = $env:GITHUB_REF_NAME; $false = $( git symbolic-ref --short -q HEAD ) }[$env:GITHUB_REF_NAME -ne $NULL]
+    $revision = @{ $true = "{0:00000}" -f [convert]::ToInt32("0" + $env:GITHUB_RUN_NUMBER, 10); $false = "local" }[$env:GITHUB_RUN_NUMBER -ne $NULL]
+    $suffix = @{ $true = ""; $false = "$($branch.Substring(0,[math]::Min(10, $branch.Length)) )-$revision" }[$branch -ne "dev" -and $revision -ne "local"]
 
-        echo "build: Testing project in $($test.FullName)"
+    echo "build: Version suffix is $suffix"
 
-        & dotnet test -c Release --collect "XPlat Code Coverage"
-        if ($LASTEXITCODE -ne 0) { exit 3 }
+    $sinkProjectPath = "$PSScriptRoot/src/Serilog.Sinks.MSSqlServer"
+    try
+    {
+        Push-Location "$sinkProjectPath"
 
+        echo "build: Packaging sink main project in $sinkProjectPath"
+        if ($suffix)
+        {
+            & dotnet pack -c Release -o ..\..\artifacts --version-suffix=$suffix
+        }
+        else
+        {
+            & dotnet pack -c Release -o ..\..\artifacts
+        }
+        if ($LASTEXITCODE -ne 0)
+        {
+            echo "Error returned by dotnet pack. Aborting build."
+            exit 1
+        }
+    }
+    finally
+    {
         Pop-Location
     }
 
-    # The performance benchmark tests should at least build without errors during PR validation
-    $perfTestProjectPath = "$PSScriptRoot/test/Serilog.Sinks.MSSqlServer.PerformanceTests"
-    Push-Location "$perfTestProjectPath"
+    if ($SkipTests -eq $false)
+    {
+        $testProjectPath = "$PSScriptRoot/test/Serilog.Sinks.MSSqlServer.Tests"
+        try
+        {
+            Push-Location "$testProjectPath"
 
-    echo "build: Building performance test project in $perfTestProjectPath"
-    & dotnet build -c Release
+            echo "build: Testing project in $testProjectPath"
+            & dotnet test -c Release --collect "XPlat Code Coverage"
+            if ($LASTEXITCODE -ne 0)
+            {
+                exit 2
+            }
 
+        }
+        finally
+        {
+            Pop-Location
+        }
+    }
+
+    if ($SkipPerfTests -eq $false)
+    {
+        # The performance benchmark tests should at least build without errors during PR validation
+        $perfTestProjectPath = "$PSScriptRoot/test/Serilog.Sinks.MSSqlServer.PerformanceTests"
+        try
+        {
+            Push-Location "$perfTestProjectPath"
+
+            echo "build: Building performance test project in $perfTestProjectPath"
+            & dotnet build -c Release
+            if ($LASTEXITCODE -ne 0)
+            {
+                exit 3
+            }
+        }
+        finally
+        {
+            Pop-Location
+        }
+    }
+
+    if ($SkipSamples -eq $false)
+    {
+        foreach ($src in Get-ChildItem "$PSScriptRoot/samples/**/*.csproj" -File)
+        {
+            try
+            {
+                Push-Location $src.DirectoryName
+
+                echo "build: Building sample project $( $src.FullName )"
+                & dotnet pack -c Release -o ..\..\artifacts
+                if ($LASTEXITCODE -ne 0)
+                {
+                    echo "Error returned by dotnet build. Aborting build."
+                    exit 4
+                }
+            }
+            finally
+            {
+                Pop-Location
+            }
+        }
+    }
+
+}
+finally
+{
     Pop-Location
 }
-
-Pop-Location
--- RunPerfTests.ps1
+++ RunPerfTests.ps1
@@ -7,24 +7,37 @@ param (
 
 echo "perf: Performance tests started with Filter = $Filter"
 
-Push-Location $PSScriptRoot
+try
+{
+    Push-Location $PSScriptRoot
 
-$artifactsPath = "$PSScriptRoot\artifacts\perftests"
+    $artifactsPath = "$PSScriptRoot\artifacts\perftests"
 
-if (Test-Path "$artifactsPath") {
-    echo "perf: Cleaning $artifactsPath"
-    Remove-Item "$artifactsPath" -Force -Recurse
+    if (Test-Path "$artifactsPath")
+    {
+        echo "perf: Cleaning $artifactsPath"
+        Remove-Item "$artifactsPath" -Force -Recurse
+    }
+
+    New-Item -Path "$artifactsPath" -ItemType Directory
+
+    $perfTestProjectPath = "$PSScriptRoot/test/Serilog.Sinks.MSSqlServer.PerformanceTests"
+    try
+    {
+        Push-Location "$perfTestProjectPath"
+
+        echo "perf: Running performance test project in $perfTestProjectPath"
+        & dotnet run -c Release -- -f $Filter
+
+        cp ".\BenchmarkDotNet.Artifacts\results\*.*" "$artifactsPath\"
+    }
+    finally
+    {
+        Pop-Location
+    }
+
 }
-
-New-Item -Path "$artifactsPath" -ItemType Directory
-
-$perfTestProjectPath = "$PSScriptRoot/test/Serilog.Sinks.MSSqlServer.PerformanceTests"
-Push-Location "$perfTestProjectPath"
-
-echo "perf: Running performance test project in $perfTestProjectPath"
-& dotnet run -c Release -- -f $Filter
-
-cp ".\BenchmarkDotNet.Artifacts\results\*.*" "$artifactsPath\"
-Pop-Location
-
-Pop-Location
+finally
+{
+    Pop-Location
+}
